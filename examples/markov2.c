17058a3b0346e52d1aecdefafd184f99a82d0c4c
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2017-11-14 14:56:28 +0100

    markov2 example: data flow in other direction as in markov
    In markov, we collecting state probabilities from incoming connections,
    to update values of owned states, using an overlapping partitioning to
    read from.
    
    Instead, in markov2, we propagate the state value owned by a task to other
    states using outgoing connections, writing to an overlapping partitioning.
    This uses automatic reductions done by LAIK on states at partition
    boundaries.
    
    TODO: Still buggy, but not working with >1 tasks anyway at the moment, as
    reduction on subsets of an index space is not implemented yet in LAIK.

---
+++ examples/markov2.c
@@ -0,0 +1,408 @@
+/* This file is part of the LAIK parallel container library.
+ * Copyright (c) 2017 Josef Weidendorfer
+ *
+ * LAIK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * LAIK is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Distributed Markov chain example, using LAIK reduction.
+ */
+
+#ifdef USE_MPI
+#include "laik-backend-mpi.h"
+#else
+#include "laik-backend-single.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+typedef struct _MGraph {
+    int n;   // number of states
+    int out;  // fan-out
+    int* cm; // connectivity
+    double* pm; // probabilities
+} MGraph;
+
+// Produce a graph with <n> nodes and some arbitrary connectivity
+// with a fan-in <in>. The resulting graph will be stored in
+// <cm>[i,c], which is a <n> * (<in> +1) matrix storing the incoming nodes
+// of node i in row i, using columns 1 .. <in> (column 0 is set to i).
+// <pm>[i,j] is initialized with the probability of the transition
+// from node <cm>[i,j] to node i, with cm[i,0] the prob for staying.
+void init(MGraph* mg, int fineGrained)
+{
+    int n = mg->n;
+    int out = mg->out;
+    int* cm = mg->cm;
+    double* pm = mg->pm;
+    double sum;
+
+    // some kind of ring structure
+    for(int i=0; i < n; i++) {
+        int step = 1;
+        cm[i * (out + 1) + 0] = i; // stay in i
+        pm[i * (out + 1) + 0] = 5;
+        sum = 5;
+        for(int j = 1; j <= out; j++) {
+            int toNode = (i + step) % n;
+            double prob = (double) ((j+i) % (5 * out)) + 1;
+            sum += prob;
+            cm[i * (out + 1) + j] = toNode;
+            pm[i * (out + 1) + j] = prob;
+            step = 2 * step + j + fineGrained * (i % 37);
+            while(step > n) step -= n;
+        }
+        // normalization: all outgoing probabilities need to sum up to 1.0
+        for(int j = 0; j <= out; j++) {
+            pm[i * (out + 1) + j] /= sum;
+        }
+    }
+}
+
+void print(MGraph* mg)
+{
+    int n = mg->n;
+    int out = mg->out;
+    int* cm = mg->cm;
+    double* pm = mg->pm;
+
+    for(int i = 0; i < n; i++) {
+        printf("State %2d: stay %.3f ", i, pm[i * (out + 1)]);
+        for(int j = 1; j <= out; j++)
+            printf("=(%.3f)=>%-2d  ",
+                   pm[i * (out + 1) + j], cm[i * (out + 1) + j]);
+        printf("\n");
+    }
+}
+
+
+void run_markovPartitioner(Laik_Partitioner* pr,
+                           Laik_BorderArray* ba, Laik_BorderArray* otherBA)
+{
+    MGraph* mg = laik_partitioner_data(pr);
+    int out = mg->out;
+    int* cm = mg->cm;
+
+    // go over states and add itself and incoming states to new partitioning
+    int sliceCount = laik_borderarray_getcount(otherBA);
+    for(int i = 0; i < sliceCount; i++) {
+        Laik_TaskSlice* ts = laik_borderarray_get_tslice(otherBA, i);
+        const Laik_Slice* s = laik_taskslice_getslice(ts);
+        int task = laik_taskslice_gettask(ts);
+        for(int st = s->from.i[0]; st < s->to.i[0]; st++) {
+            int off = st * (out + 1);
+            // j=0: state itself
+            for(int j = 0; j <= out; j++)
+                laik_append_index_1d(ba, task, cm[off + j]);
+        }
+    }
+}
+
+
+
+// iteratively calculate probability distribution, return last written data
+// this version expects one (sparse) mapping of data1/data2 each
+Laik_Data* runSparse(MGraph* mg, int miter,
+                     Laik_Data* data1, Laik_Data* data2,
+                     Laik_Partitioning* pWrite, Laik_Partitioning* pRead)
+{
+    int out = mg->out;
+    int* cm = mg->cm;
+    double* pm = mg->pm;
+
+    // start reading from data1, writing to data2
+    Laik_Data *dRead = data1, *dWrite = data2;
+    double *src, *dst;
+    uint64_t srcCount, dstCount, srcFrom, srcTo, dstFrom;
+
+    int iter = 0;
+    while(1) {
+        // switch dRead to pRead, dWrite to pWrite
+        laik_switchto(dRead,  pRead,  LAIK_DF_CopyIn);
+        laik_map_def1(dRead, (void**) &src, &srcCount);
+        laik_my_slice_1d(pRead, 0, &srcFrom, &srcTo);
+        assert(srcCount == srcTo - srcFrom);
+
+        laik_switchto(dWrite, pWrite,
+                      LAIK_DF_Init | LAIK_DF_ReduceOut | LAIK_DF_Sum);
+        laik_map_def1(dWrite, (void**) &dst, &dstCount);
+        dstFrom = laik_local2global_1d(dWrite, 0);
+
+        // spread values according to probability distribution
+        for(int i = srcFrom; i < srcTo; i++) {
+            int off = i * (out + 1);
+            dst[i - dstFrom] = src[i - srcFrom] * pm[off];
+            for(int j = 1; j <= out; j++)
+                dst[cm[off + j] - dstFrom] += src[i - srcFrom] * pm[off + j];
+        }
+
+        iter++;
+        if (iter == miter) break;
+
+        // swap role of data1 and data2
+        if (dRead == data1) { dRead = data2; dWrite = data1; }
+        else                { dRead = data1; dWrite = data2; }
+    }
+
+    return dWrite;
+}
+
+// iteratively calculate probability distribution, return last written data
+// this assumes a compact mapping for data1/2, using indirection
+Laik_Data* runIndirection(MGraph* mg, int miter,
+                          Laik_Data* data1, Laik_Data* data2, Laik_Data* idata,
+                          Laik_Partitioning* pWrite, Laik_Partitioning* pRead)
+{
+    int out = mg->out;
+    double* pm = mg->pm;
+
+    // local index array
+    int* iarray;
+    uint64_t icount;
+    laik_map_def1(idata, (void**) &iarray, &icount);
+
+    // start reading from data1, writing to data2
+    Laik_Data *dRead = data1, *dWrite = data2;
+    double *src, *dst;
+    uint64_t srcCount, dstCount, srcFrom, srcTo, dstFrom;
+
+    int iter = 0;
+    while(1) {
+        // switch dRead to pRead, dWrite to pWrite
+        laik_switchto(dRead,  pRead,  LAIK_DF_CopyIn);
+        laik_map_def1(dRead, (void**) &src, &srcCount);
+        laik_my_slice_1d(pRead, 0, &srcFrom, &srcTo);
+        assert(srcCount == srcTo - srcFrom);
+
+        laik_switchto(dWrite, pWrite,
+                      LAIK_DF_Init | LAIK_DF_ReduceOut | LAIK_DF_Sum);
+        laik_map_def1(dWrite, (void**) &dst, &dstCount);
+        dstFrom = laik_local2global_1d(dWrite, 0);
+
+        // spread values according to probability distribution
+        for(int i = 0; i < srcCount; i++) {
+            int off = i * (out + 1);
+            int goff = (i + srcFrom) * (out + 1);
+            dst[iarray[off]] = src[i] * pm[goff];
+            for(int j = 1; j <= out; j++)
+                dst[iarray[off + j]] += src[i] * pm[goff + j];
+        }
+
+        iter++;
+        if (iter == miter) break;
+
+        // swap role of data1 and data2
+        if (dRead == data1) { dRead = data2; dWrite = data1; }
+        else                { dRead = data1; dWrite = data2; }
+    }
+
+    return dWrite;
+}
+
+
+int main(int argc, char* argv[])
+{
+#ifdef USE_MPI
+    Laik_Instance* inst = laik_init_mpi(&argc, &argv);
+#else
+    Laik_Instance* inst = laik_init_single();
+#endif
+    Laik_Group* world = laik_world(inst);
+
+    int n = 1000000;
+    int out = 10;
+    int miter = 10;
+    int doPrint = 0;
+    int doCompact = 0;
+    int doIndirection = 0;
+    int useSingleIndex = 0;
+    int fineGrained = 0;
+
+    int arg = 1;
+    while((arg < argc) && (argv[arg][0] == '-')) {
+        if (argv[arg][1] == 'c') doCompact = 1;
+        if (argv[arg][1] == 'i') doIndirection = 1;
+        if (argv[arg][1] == 's') useSingleIndex = 1;
+        if (argv[arg][1] == 'f') fineGrained = 1;
+        if (argv[arg][1] == 'p') doPrint = 1;
+        if (argv[arg][1] == 'h') {
+            printf("markov [options] [<statecount> [<fan-out> [<iterations>]]]\n"
+                   "\nOptions:\n"
+                   " -i: use indirection with pre-calculated local indexes\n"
+                   " -c: use a compact mapping (implies -i)\n"
+                   " -s: use single index hint\n"
+                   " -f: use pseudo-random connectivity (much more slices)\n"
+                   " -p: print connectivity\n"
+                   " -h: this help text\n");
+            exit(1);
+        }
+        arg++;
+    }
+    if (argc > arg) n = atoi(argv[arg]);
+    if (argc > arg + 1) out = atoi(argv[arg + 1]);
+    if (argc > arg + 2) miter = atoi(argv[arg + 2]);
+
+    if (n == 0) n = 1000000;
+    if (out == 0) out = 10;
+    if (doCompact) doIndirection = 1;
+
+    if (laik_myid(world) == 0) {
+        printf("Init Markov chain with %d states, max fan-out %d\n", n, out);
+        printf("Run %d iterations each.%s%s%s\n", miter,
+               useSingleIndex ? " Partitioner using single indexes.":"",
+               doCompact ? " Using compact mapping.":"",
+               doIndirection ? " Using indirection.":"");
+    }
+
+    MGraph mg;
+    mg.n = n;
+    mg.out = out;
+    mg.cm = malloc(n * (out + 1) * sizeof(int));
+    mg.pm = malloc(n * (out + 1) * sizeof(double));
+
+    init(&mg, fineGrained);
+    if (doPrint) print(&mg);
+
+    // two 1d arrays, using same space
+    Laik_Space* space = laik_new_space_1d(inst, n);
+    Laik_Data* data1 = laik_new_data(world, space, laik_Double);
+    Laik_Data* data2 = laik_new_data(world, space, laik_Double);
+
+    // partitionings:
+    // - pRead  : distributes the states owned by tasks, propagating from
+    // - pWrite : provides access to out-going states to propagate values to
+    // - pMaster: all data at master, for checksum
+    // pRead/pWrite partitionings are assigned to either data1/data2,
+    // exchanged after every iteration
+    Laik_Partitioning *pRead, *pWrite, *pMaster;
+    Laik_Partitioner* pr;
+    pRead = laik_new_partitioning(world, space,
+                                  laik_new_block_partitioner1(), 0);
+    pr = laik_new_partitioner("markov-out", run_markovPartitioner, &mg,
+                              LAIK_PF_Merge |
+                              (useSingleIndex ? LAIK_PF_SingleIndex : 0) |
+                              (doCompact ? LAIK_PF_Compact : 0));
+    pWrite = laik_new_partitioning(world, space, pr, pRead);
+    pMaster = laik_new_partitioning(world, space, laik_Master, 0);
+
+    // for indirection, we store local indexes in a LAIK container
+    Laik_Type* itype = laik_register_type("l-indexes", (out + 1) * sizeof(int));
+    Laik_Data* idata = laik_new_data(world, space, itype);
+
+    if (doIndirection) {
+        // register initialization function for global-to-local index data
+        // this is called whenever the partitioning is changing
+        // FIXME: add API to specify function for init
+        laik_switchto(idata, pRead, 0);
+        // TODO: move to inititialization function
+        int* iarray;
+        uint64_t icount, ioff;
+        laik_map_def1(idata, (void**) &iarray, &icount);
+        for(uint64_t i = 0; i < icount; i++) {
+            int gi = laik_local2global_1d(idata, i);
+            for(int j = 0; j <= out; j++) {
+                int gidx = mg.cm[gi * (out + 1) + j];
+                assert(laik_global2local_1d(idata, gidx, &ioff) != 0);
+                iarray[i * (out + 1) + j] = ioff;
+            }
+        }
+    }
+
+    if (laik_myid(world) == 0)
+        printf("Start with state 0 prob 1 ...\n");
+
+    // distributed initialization of data1
+    // (uses pRead with disjuntive partitioning here, in contrast to reading
+    //  from owned states later in the iterations)
+    double *v;
+    uint64_t count, off;
+    laik_switchto(data1, pRead, LAIK_DF_CopyOut);
+    laik_map_def1(data1, (void**) &v, &count);
+    for(uint64_t i = 0; i < count; i++)
+        v[i] = (double) 0.0;
+    // set state 0 to probability 1
+    if (laik_global2local_1d(data1, 0, &off)) {
+        // if global index 0 is local, it must be at local index 0
+        assert(off == 0);
+        v[off] = 1.0;
+    }
+
+    Laik_Data* dRes;
+    if (doIndirection)
+        dRes = runIndirection(&mg, miter, data1, data2, idata, pWrite, pRead);
+    else
+        dRes = runSparse(&mg, miter, data1, data2, pWrite, pRead);
+
+    laik_switchto(dRes, pMaster, LAIK_DF_CopyIn);
+    laik_map_def1(dRes, (void**) &v, &count);
+    if (laik_myid(world) == 0) {
+        assert(count == n);
+        double sum = 0.0;
+        for(int i=0; i < n; i++)
+            sum += v[i];
+        printf("  result probs: p0 = %g, p1 = %g, p2 = %g, Sum: %f\n",
+               v[0], v[1], v[2], sum);
+    }
+
+    if (laik_myid(world) == 0)
+        printf("Start with state 1 prob 1 ...\n");
+    laik_switchto(data1, pRead, LAIK_DF_CopyOut);
+    laik_map_def1(data1, (void**) &v, &count);
+    for(uint64_t i = 0; i < count; i++)
+        v[i] = (double) 0.0;
+    if (laik_global2local_1d(data1, 1, &off))
+        v[off] = 1.0;
+
+    if (doIndirection)
+        dRes = runIndirection(&mg, miter, data1, data2, idata, pWrite, pRead);
+    else
+        dRes = runSparse(&mg, miter, data1, data2, pWrite, pRead);
+
+    laik_switchto(dRes, pMaster, LAIK_DF_CopyIn);
+    laik_map_def1(dRes, (void**) &v, &count);
+    if (laik_myid(world) == 0) {
+        double sum = 0.0;
+        for(int i=0; i < n; i++)
+            sum += v[i];
+        printf("  result probs: p0 = %g, p1 = %g, p2 = %g, Sum: %f\n",
+               v[0], v[1], v[2], sum);
+    }
+
+    if (laik_myid(world) == 0)
+        printf("Start with all probs equal ...\n");
+    laik_switchto(data1, pRead, LAIK_DF_CopyOut);
+    laik_map_def1(data1, (void**) &v, &count);
+    double p = 1.0 / n;
+    for(uint64_t i = 0; i < count; i++)
+        v[i] = p;
+
+    if (doIndirection)
+        dRes = runIndirection(&mg, miter, data1, data2, idata, pWrite, pRead);
+    else
+        dRes = runSparse(&mg, miter, data1, data2, pWrite, pRead);
+
+    laik_switchto(dRes, pMaster, LAIK_DF_CopyIn);
+    laik_map_def1(dRes, (void**) &v, &count);
+    if (laik_myid(world) == 0) {
+        double sum = 0.0;
+        for(int i=0; i < n; i++)
+            sum += v[i];
+        printf("  result probs: p0 = %g, p1 = %g, p2 = %g, Sum: %f\n",
+               v[0], v[1], v[2], sum);
+    }
+
+    laik_finalize(inst);
+    return 0;
+}
