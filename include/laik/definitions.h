6b8a57d030921186ef6c40825289b9d625c9506e
Dai <me@daiyang.de>
2017-11-15 14:56:09 +0100

    wip2

---
+++ include/laik/definitions.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the LAIK parallel container library.
+ * Copyright (c) 2017 Dai Yang
+ *
+ * LAIK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * LAIK is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define MAX_GROUPS        10
+#define MAX_SPACES        10
+#define MAX_PARTITIONINGS 10
+#define MAX_DATAS         10
+#define MAX_MAPPINGS      10
+#define MAX_AGENTS        10
+#define MAX_FILENAME_LENGTH 128
