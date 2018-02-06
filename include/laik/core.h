envelope-project/laik

commit 72f26412a88bb055baf84166a0bf43af7346cfd0

src/core.c: Make laik_new_instance() create a first group with the same parameters as the instance.
This means, that the individual backend's laik_init_*() functions only
have to create the instance (as the type signature suggests) and can be
oblivious to the concept of groups.

src/backend-mpi.c
@@ -100,22 +100,6 @@ Laik_Instance* laik_init_mpi(int* argc, char*** argv)
 
     sprintf(inst->guid, "%d", rank);
 
-    // group world
-
-    MPIGroupData* gd = malloc(sizeof(MPIGroupData));
-    if (!gd) {
-        laik_panic("Out of memory allocating MPIGroupData object");
-        exit(1); // not actually needed, laik_panic never returns
-    }
-    gd->comm = MPI_COMM_WORLD;
-
-    Laik_Group* g = laik_create_group(inst);
-    g->inst = inst;
-    g->gid = 0;
-    g->size = inst->size;
-    g->myid = inst->myid;
-    g->backend_data = gd;
-
     laik_log(1, "MPI backend initialized (location '%s', pid %d)\n",
              inst->mylocation, (int) getpid());
 

src/backend-single.c
@@ -31,13 +31,6 @@ Laik_Instance* laik_init_single()
     Laik_Instance* inst;
     inst = laik_new_instance(&laik_backend_single, 1, 0, "local", 0);
 
-    // group world
-    Laik_Group* g = laik_create_group(inst);
-    g->inst = inst;
-    g->gid = 0;
-    g->size = 1;
-    g->myid = 0;
-
     laik_log(1, "Single backend initialized\n");
 
     single_instance = inst;

src/core.c
@@ -136,6 +136,13 @@ Laik_Instance* laik_new_instance(const Laik_Backend* b,
         stdout = laik_logfile;
     }
 
+    // Create a group in this instance with same parameters as the instance.
+    // Since it's the first group, this is what laik_world() will return.
+    Laik_Group* first_group = laik_create_group (instance);
+    first_group->size         = size;
+    first_group->myid         = myid;
+    first_group->backend_data = data;
+
     return instance;
 }
 

commit 9f641a2c9a042c26d79cc26ff415a90a3c69dcef

Fix bug introduced in PR #76
laik_new_instance() needs to also pass backend-specific
group data to create the first group. E.g. for the MPI
backend, PR#76 mixed up MPIData and MPIGroupData.

include/laik/core.h
@@ -42,7 +42,7 @@ typedef struct _Laik_Group Laik_Group;
 
 // allocate space for a new LAIK instance
 Laik_Instance* laik_new_instance(const Laik_Backend* b, int size, int myid,
-                                 char* location, void* data);
+                                 char* location, void* data, void *gdata);
 
 //! shut down communication and free resources of this instance
 void laik_finalize(Laik_Instance* inst);

src/backend-mpi.c
@@ -80,6 +80,14 @@ Laik_Instance* laik_init_mpi(int* argc, char*** argv)
     d->didInit = false;
     d->comm = MPI_COMM_WORLD;
 
+    MPIGroupData* gd = malloc(sizeof(MPIGroupData));
+    if (!gd) {
+        laik_panic("Out of memory allocating MPIGroupData object");
+        exit(1); // not actually needed, laik_panic never returns
+    }
+    gd->comm = MPI_COMM_WORLD;
+
+
     if (argc) {
         MPI_Init(argc, argv);
         d->didInit = true;
@@ -96,7 +104,7 @@ Laik_Instance* laik_init_mpi(int* argc, char*** argv)
 
     Laik_Instance* inst;
     inst = laik_new_instance(&laik_backend_mpi, size, rank,
-                             processor_name, d);
+                             processor_name, d, gd);
 
     sprintf(inst->guid, "%d", rank);
 

src/backend-single.c
@@ -29,7 +29,7 @@ Laik_Instance* laik_init_single()
         return single_instance;
 
     Laik_Instance* inst;
-    inst = laik_new_instance(&laik_backend_single, 1, 0, "local", 0);
+    inst = laik_new_instance(&laik_backend_single, 1, 0, "local", 0, 0);
 
     laik_log(1, "Single backend initialized\n");
 

src/core.c
@@ -75,7 +75,7 @@ char* laik_mylocation(Laik_Instance* inst)
 // allocate space for a new LAIK instance
 Laik_Instance* laik_new_instance(const Laik_Backend* b,
                                  int size, int myid,
-                                 char* location, void* data)
+                                 char* location, void* data, void *gdata)
 {
     Laik_Instance* instance;
     instance = malloc(sizeof(Laik_Instance));
@@ -141,7 +141,7 @@ Laik_Instance* laik_new_instance(const Laik_Backend* b,
     Laik_Group* first_group = laik_create_group (instance);
     first_group->size         = size;
     first_group->myid         = myid;
-    first_group->backend_data = data;
+    first_group->backend_data = gdata;
 
     return instance;
 }