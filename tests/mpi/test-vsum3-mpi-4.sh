e788b79821eca2578daacee850fd3f1a1248dee5
Alexander Kurtz <alexander@kurtz.be>
2018-02-22 15:38:43 +0100

    tests/mpi/*.sh: Make the tests work independent of the system locale. (#96)

--- tests/mpi/test-vsum3-mpi-4.sh
+++ tests/mpi/test-vsum3-mpi-4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-LAIK_BACKEND=mpi mpirun -np 4 ../../examples/vsum3 | sort > test-vsum3-mpi-4.out
+LAIK_BACKEND=mpi mpirun -np 4 ../../examples/vsum3 | LC_ALL='C' sort > test-vsum3-mpi-4.out
 cmp test-vsum3-mpi-4.out "$(dirname -- "${0}")/test-vsum.expected"
