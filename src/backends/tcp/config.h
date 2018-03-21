09882ad259e9ec8934c79ec992fe0d74facd9242
Alexander Kurtz <alexander@kurtz.be>
2018-03-21 20:24:53 +0100

    src/backends/tcp/config.{c,h}: Allow configuring all performance-relevant parameters.

--- src/backends/tcp/config.h
+++ src/backends/tcp/config.h
@@ -1,9 +1,25 @@
 #pragma once
 
-#include <glib.h>  // for GPtrArray, G_DEFINE_AUTOPTR_CLEANUP_FUNC
+#include <glib.h>    // for GPtrArray, G_DEFINE_AUTOPTR_CLEANUP_FUNC
+#include <stddef.h>  // for size_t
 
 typedef struct {
     GPtrArray* addresses;
+    double     client_activation_timeout;
+    size_t     client_connection_limit;
+    double     client_connection_timeout;
+    double     server_activation_timeout;
+    size_t     server_connection_limit;
+    double     server_connection_timeout;
+    size_t     socket_backlog;
+    size_t     socket_keepcnt;
+    double     socket_keepidle;
+    double     socket_keepintvl;
+    size_t     inbox_size_limit;
+    size_t     outbox_size_limit;
+    double     add_retry_timeout;
+    double     get_first_timeout;
+    double     get_retry_timeout;
 
     int references;
 } Laik_Tcp_Config;
