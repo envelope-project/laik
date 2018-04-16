fb30b104c68b84ea08862a3356bdd7fe4bd09183
Alexander Kurtz <alexander@kurtz.be>
2018-04-16 19:54:59 +0200

    src/backends/tcp/*.{c,h}: Add individual LGPL3+ license headers.
    Closes #126.

--- src/backends/tcp/backend.c
+++ src/backends/tcp/backend.c
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the LAIK library.
+ * Copyright (c) 2018 Alexander Kurtz <alexander@kurtz.be>
+ *
+ * LAIK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3 or later.
+ *
+ * LAIK is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <glib.h>           // for g_malloc0_n, g_malloc, g_autoptr, g_new0
 #include <laik-internal.h>  // for _Laik_Data, _Laik_Group, redTOp, _Laik_Ma...
 #include <laik.h>           // for Laik_Data, Laik_Group, Laik_Mapping, Laik...
--- src/backends/tcp/socketqueue.c
+++ src/backends/tcp/socketqueue.c
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the LAIK library.
+ * Copyright (c) 2018 Alexander Kurtz <alexander@kurtz.be>
+ *
+ * LAIK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3 or later.
+ *
+ * LAIK is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "socketqueue.h"
 #include <glib.h>        // for GPtrArray, GArray, g_array_new, g_array_remo...
 #include <poll.h>        // for pollfd, poll, POLLIN, POLLNVAL
