1730afc523524b77367dca59916b5f6d5b7c7914
Vinpasso <vincent@jd-bode.de>
2019-05-28 15:32:37 +0200

    Add possibility to clear error status

--- include/laik-backend-tcp.h
+++ include/laik-backend-tcp.h
@@ -6,3 +6,5 @@ Laik_Instance* laik_init_tcp (int* argc, char*** argv);
 
 typedef void (*LaikTCPErrorHandler)(void*);
 void laik_tcp_set_error_handler(LaikTCPErrorHandler newErrorHandler);
+int laik_tcp_get_status();
+void laik_tcp_clear_errors();
