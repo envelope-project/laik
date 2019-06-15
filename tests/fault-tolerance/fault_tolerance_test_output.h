7f1ea108da453b49766ff23831ccd777ee9a23b7
Vinpasso <vincent@jd-bode.de>
2019-06-15 13:31:43 +0200

    Further improvements to pgm output

--- tests/fault-tolerance/fault_tolerance_test_output.h
+++ tests/fault-tolerance/fault_tolerance_test_output.h
@@ -21,20 +21,21 @@ void writeDataToFile(char *fileNamePrefix, char *fileNameExtension, Laik_Data *d
     assert(data->activeMappings->count == 1);
 
     double *base = (double *) data->activeMappings->map[0].base;
-    uint64_t stride = data->activeMappings->map[0].size[1];
     uint64_t dim0Size = data->activeMappings->map[0].size[0];
+    uint64_t dim1Size = data->activeMappings->map[0].size[1];
+    uint64_t stride = data->activeMappings->map[0].layout->stride[1];
 
     uint64_t count = data->activeMappings->map[0].count;
-    assert(dim0Size * stride == count);
+    assert(dim0Size * dim1Size == count);
 
-    fprintf(myOutput, "P2\n%lu %lu\n%i", stride, dim0Size, 255);
+    fprintf(myOutput, "P2\n%lu %lu\n%i", dim1Size, dim0Size, 255);
 
-    for (unsigned long i = 0; i < count; ++i) {
-        if (i % stride == 0) {
-            fprintf(myOutput, "\n");
+    for (unsigned long y = 0; y < dim1Size; ++y) {
+        fprintf(myOutput, "\n");
+        for (unsigned long x = 0; x < dim0Size; ++x) {
+            unsigned char value = (unsigned char) (base[y * stride + x] * 255);
+            fprintf(myOutput, "%i ", value);
         }
-        unsigned char value = (unsigned char) (base[i] * 255);
-        fprintf(myOutput, "%i ", value);
     }
 //    if(fwrite(base, data->type->size, data->activeMappings->map[0].count, myOutput)) {
 //        assert(0);
