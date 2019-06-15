072f6c92e143640f311ab39b75dbfce103d3bb25
Vinpasso <vincent@jd-bode.de>
2019-06-15 13:18:56 +0200

    Improvements to pgm output

--- tests/fault-tolerance/fault_tolerance_test_output.h
+++ tests/fault-tolerance/fault_tolerance_test_output.h
@@ -24,22 +24,17 @@ void writeDataToFile(char *fileNamePrefix, char *fileNameExtension, Laik_Data *d
     uint64_t stride = data->activeMappings->map[0].size[1];
     uint64_t dim0Size = data->activeMappings->map[0].size[0];
 
-    int max = -1;
     uint64_t count = data->activeMappings->map[0].count;
-    for (unsigned long i = 0; i < count; ++i) {
-        if(max < (unsigned char) (base[i] * 255)) {
-            max = (unsigned char) (base[i] * 255);
-        }
-    }
     assert(dim0Size * stride == count);
 
-    fprintf(myOutput, "P2\n%lu %lu\n%i", dim0Size, stride, max);
+    fprintf(myOutput, "P2\n%lu %lu\n%i", stride, dim0Size, 255);
 
     for (unsigned long i = 0; i < count; ++i) {
         if (i % stride == 0) {
             fprintf(myOutput, "\n");
         }
-        fprintf(myOutput, "%i ", (unsigned char) (base[i] * 255));
+        unsigned char value = (unsigned char) (base[i] * 255);
+        fprintf(myOutput, "%i ", value);
     }
 //    if(fwrite(base, data->type->size, data->activeMappings->map[0].count, myOutput)) {
 //        assert(0);
