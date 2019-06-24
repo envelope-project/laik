59d31ce8e20a1d5493a5d1d318ea18e056cdf72d
Vinpasso <vincent@jd-bode.de>
2019-06-24 13:45:07 +0200

    Update fault tolerance checkpoint mechanic

--- include/laik-fault-tolerance.h
+++ include/laik-fault-tolerance.h
@@ -17,7 +17,7 @@ struct _Laik_Checkpoint {
 
 typedef struct _Laik_Checkpoint Laik_Checkpoint;
 
-Laik_Checkpoint laik_checkpoint_create(Laik_Instance *laikInstance, Laik_Space *space, Laik_Data *data,
+Laik_Checkpoint* laik_checkpoint_create(Laik_Instance *laikInstance, Laik_Space *space, Laik_Data *data,
                                        Laik_Partitioner *backupPartitioner, Laik_Group *backupGroup,
                                        enum _Laik_ReductionOperation reductionOperation);
 
