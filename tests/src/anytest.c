8122934df9fd79664e3b7e28a0b7fe2e7ce13235
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2019-07-18 18:59:52 +0200

    Add test code for reduction LAIK_RO_ANY
    Not working yet - we need a actionseq translation pass to
    make simple copies out of it, taking an input which minimizes
    communication (e.g. if locally available, use local version).

---
+++ tests/src/anytest.c
@@ -0,0 +1,40 @@
+#include "laik.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    double *base;
+    uint64_t count, i;
+
+    Laik_Instance*     inst  = laik_init(&argc, &argv);
+    Laik_Group*        world = laik_world(inst);
+    Laik_Space*        space = laik_new_space_1d(inst, 10000);
+    Laik_Data*         data  = laik_new_data(space, laik_Double);
+    Laik_Partitioning* block = laik_new_partitioning(laik_new_block_partitioner1(),
+                                                     world, space, 0);
+    Laik_Partitioning* halo  = laik_new_partitioning(laik_new_cornerhalo_partitioner(1),
+                                                     world, space, block);
+
+    laik_switchto_partitioning(data, block, 0, 0);
+    laik_fill_double(data, 1.0);
+    laik_switchto_partitioning(data, halo, LAIK_DF_Preserve, LAIK_RO_Any);
+
+    // go over own data and double each value
+    laik_map_def1(data, (void**) &base, &count);
+    for (i = 0; i < count; i++) base[i] *= 2.0;
+
+    laik_switchto_partitioning(data, halo, LAIK_DF_Preserve, LAIK_RO_Any);
+
+    laik_map_def1(data, (void**) &base, &count);
+    for (i = 0; i < count; i++) base[i] *= 2.0;
+
+    laik_switchto_new_partitioning(data, world, laik_Master,
+                                   LAIK_DF_Preserve, LAIK_RO_Sum);
+    if (laik_myid(world) == 0) {
+        double sum = 0.0;
+        laik_map_def1(data, (void**) &base, &count);
+        for (i = 0; i < count; i++) sum += base[i];
+        printf("Result: %f\n", sum);
+    }
+    laik_finalize(inst);
+}
