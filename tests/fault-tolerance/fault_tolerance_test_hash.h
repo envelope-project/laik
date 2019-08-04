29a2f8cdecb5004706f4b7741918c659e7d9a442
Vinpasso <vincent@jd-bode.de>
2019-08-04 15:44:39 +0200

    Replace hash with mock (remove dependency on OpenSSL)

--- tests/fault-tolerance/fault_tolerance_test_hash.h
+++ tests/fault-tolerance/fault_tolerance_test_hash.h
@@ -5,8 +5,14 @@
 #ifndef LAIK_FAULT_TOLERANCE_TEST_HASH_H
 #define LAIK_FAULT_TOLERANCE_TEST_HASH_H
 
-#include <openssl/sha.h>
 #include <stdio.h>
+
+#define SHA_DIGEST_LENGTH 1
+
+void SHA1(const unsigned char* base, uint64_t length, unsigned char* hash) {
+    (void)base; (void)length;
+    hash[0] = -1;
+}
 
 void test_hexHash(char *msg, void *baseAddress, uint64_t length, unsigned char *hash) {
     SHA1((const unsigned char *) baseAddress, length, hash);
