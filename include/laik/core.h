c0ce136af9739013a9ff79a7018e14b3b154944a
Vinpasso <vincent@jd-bode.de>
2019-08-07 15:09:01 +0200

    Add matlab render code and change tracing format

--- include/laik/core.h
+++ include/laik/core.h
@@ -238,8 +238,6 @@ double getNSize(Laik_Instance *inst);
 int getEventNum();
 int eventNum;
 #define TRACE_EVENT(name, format, ...) printf("===,EVENT,%s,RANK,%i,AT,%f,USAGE,%f: : " format " ===\n", name, laik_myid(laik_world(inst)), getTime(inst), getVSize(), __VA_ARGS__)
-#define TRACE_EVENT_S(name, format) printf("===,EVENT,%i,%s,RANK,%i,AT,%f,MEM,%fKB,NET,%fKB,: " format " ===\n", getEventNum(), name, laik_myid(laik_world(inst)), getTime(inst), getVSize(), getNSize(inst))
-
-
+#define TRACE_EVENT_S(name, format) printf("===,EVENT,%i,%s,RANK,%i,AT,%f,MEM,%f,KB,NET,%f,KB,: " format " ===\n", getEventNum(), name, laik_myid(laik_world(inst)), getTime(inst), getVSize(), getNSize(inst))
 
 #endif // LAIK_CORE_H
---
+++ renderTimeline.m
@@ -0,0 +1,36 @@
+
+output = readtable('output.txt');
+output = sortrows(output, [6, 8]);
+
+figure();
+scatter(output.Var8, output.Var6 + 1);
+label(output.Var4, output.Var8, output.Var6 + 1, output.Var6);
+title('Timeline');
+
+figure();
+for rank = 1 : numel(unique(output.Var6))    
+    plot(output.Var8(output.Var6 == rank, :), output.Var10(output.Var6 == rank, :));
+    label(output.Var4(output.Var6 == rank, :), output.Var8(output.Var6 == rank, :), output.Var10(output.Var6 == rank, :), output.Var6(output.Var6 == rank, :));
+end
+title('MEM Usage');
+
+figure();
+for rank = 1 : numel(unique(output.Var6))    
+    plot(output.Var8(output.Var6 == rank, :), output.Var13(output.Var6 == rank, :));
+    label(output.Var4(output.Var6 == rank, :), output.Var8(output.Var6 == rank, :), output.Var13(output.Var6 == rank, :), output.Var6(output.Var6 == rank, :));
+end
+title('NET Usage');
+
+function label(labels, xVals, yVals, ranks)
+    displs = linspace(0, 0, numel(xVals));
+    %// Assign labels.
+    for labelID = 1 : numel(xVals)
+        if(labelID > 2 && abs(xVals(labelID) - xVals(labelID - 1)) > 0.1)
+            displs(ranks(labelID) + 1) = 0.05;
+        else
+            displs(ranks(labelID) + 1) = displs(ranks(labelID) + 1) + 0.05;
+        end
+       text(xVals(labelID), yVals(labelID) - displs(ranks(labelID) + 1), labels(labelID), 'HorizontalAlignment', 'left');
+       line([xVals(labelID), xVals(labelID)], [yVals(labelID) - displs(ranks(labelID) + 1), yVals(labelID)]);
+    end
+end
