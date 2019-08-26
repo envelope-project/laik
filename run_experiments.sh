e748bef5f97e31d28deb371e76b03efba3c3366f
Vinpasso <vincent@jd-bode.de>
2019-08-26 11:56:57 +0200

    Improvements to experiment script for pi cluster

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+#SBATCH -p rpi # partition (queue)
+###SBATCH -N 32 # number of nodes
+#SBATCH -n 32 # number of tasks
+###SBATCH --mem 100 # memory pool for all cores
+#SBATCH -t 0-0:10 # time (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+
+export PATH="/u/home/bodev/lib/bin:$PATH"
 
 # $1: Name, $2 Type, $3 Executable, $4 Executable_args, $5 run_number
 run_experiment () {
 	echo "Running experiment $1"
-	export MPI_OPTIONS="--output-filename out --merge-stderr-to-stdout --stdin none --oversubscribe"
+	export MPI_OPTIONS="--output-filename out --merge-stderr-to-stdout --stdin none"
+#	export MPI_OPTIONS="$MPI_OPTIONS --oversubscribe"
 	export TEST_NAME="$1"
 	rm -r out/
 	tests/fault-tolerance/launcher.sh "$2" "$5" "$5" release "$3" "$4"
@@ -11,7 +21,15 @@ run_experiment () {
 	grep -h -e '!!!' out/1/rank.*/stdout > "laik_experiments/data/experiment_$1_header.csv"
 }
 
-JAC2D=cmake-build-debug_wsl/tests/fault-tolerance/checkpoint-jac2d-recovery
+PREFIX="."
+#PREFIX=cmake-build-debug_wsl
+JAC2D=$PREFIX/tests/fault-tolerance/checkpoint-jac2d-recovery
+
+if [ ! -f "$JAC2D" ]
+then
+  echo "MISSING Executable"
+  exit 255
+fi
 
 for num in 01 02 04 08 16 32
 do
@@ -21,6 +39,8 @@ done
 
 for num in 01 02 04 08 16 32
 do
-        run_experiment "mpi_weak_scale_$num" "mpi" "$JAC2D" "$(expr 256 \* $num) 50 -1 1 1" "$num"
+        PROBLEM_SIZE="$(awk "BEGIN {printf \"%.0f\n\", sqrt(65536 * $num)}")"
+        echo "Weak scaling test with problem size $PROBLEM_SIZE"
+        run_experiment "mpi_weak_scale_$num" "mpi" "$JAC2D" "$PROBLEM_SIZE 50 -1 1 1" "$num"
 done
 
