940de358f5f814e1aaf45c7ad0e2ccfb7b14f8f2
Vinpasso <vincent@jd-bode.de>
2019-10-14 16:22:44 +0200

    Update supermuc tools for maimuc

--- supermuc_tools.sh
+++ supermuc_tools.sh
@@ -16,4 +16,47 @@ case "$1" in
   "sh")
     ssh -o ProxyCommand="ssh -W %h:%p bodev@lxhalle.in.tum.de"  ga26poh3@skx.supermuc.lrz.de
   ;;
+  "maimuc-sync-up")
+    rsync -av -e "ssh -J bodev@himmuc.caps.in.tum.de" --exclude ".*" --exclude "cmake-build-*" --exclude="out" --exclude "lib" --exclude "laik_experiments/data" ./ login@maimuc.caps.in.tum.de:/home/pi/ga26poh/laik/
+  ;;
+  "maimuc-sync-down-data")
+    rsync -av -e "ssh -J bodev@himmuc.caps.in.tum.de" ga26poh3@skx.supermuc.lrz.de:/dss/dsshome1/08/ga26poh3/laik/laik_experiments/data ./laik_experiments/
+  ;;
+  "maimuc-sync-data")
+    for i in 0 1 3 4 5 6 7 8 9
+    do
+      echo "Sync to $i"
+      rsync -avr --info=progress2 --delete ~/ga26poh mai0$i:~/
+    done
+    ;;
+  "maimuc-sync-python")
+    for i in 0 1 3 4 5 6 7 8 9
+    do
+      echo "Sync to $i"
+      rsync -a --info=progress2 --delete ~/.local mai0$i:~/
+    done
+    ;;
+  "maimuc-all-other")
+    for i in 0 1 3 4 5 6 7 8 9
+    do
+      echo "Run on $i: $2"
+      ssh mai0$i "$2"
+    done
+    ;;
+  "maimuc-ssh-apt-all")
+    for i in 0 1 3 4 5 6 7 8 9
+    do
+      echo "Run on $i: $2"
+      ssh-apt mai0$i "$2"
+    done
+    ;;
+  "maimuc-compile-and-sync")
+    cd cmake-build-debug_wsl || exit
+    make checkpoint-jac2d-recovery
+    cd .. || exit
+    ./supermuc_tools.sh maimuc-sync-data
+    ;;
+  *)
+    echo "Unrecognized: $1"
+    ;;
 esac
