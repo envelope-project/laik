b41a4abba3ff3052095193cec3b4af2abe0febae
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2019-10-16 00:28:59 +0200

    Fix spacestest.c to compile on macOS

--- tests/src/spacestest.c
+++ tests/src/spacestest.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <inttypes.h>
 
 void print_spaces(Laik_Instance* i, int step)
 {
@@ -13,19 +14,22 @@ void print_spaces(Laik_Instance* i, int step)
     s = laik_spacestore_get(i, "1d-space");
     assert(s && (s->dims == 1));
     assert(s->s.to.i[0] == ((step == 1) ? 100 : 1000));
-    printf("T%d: 1d-space: %ld - %ld\n", myid,
+    printf("T%d: 1d-space: %" PRId64 " - %" PRId64 "\n", myid,
            s->s.from.i[0], s->s.to.i[0]);
 
     s = laik_spacestore_get(i, "2d-space");
     assert(s && (s->dims == 2));
     assert(s->s.to.i[0] == ((step == 1) ? 200 : 2000));
-    printf("T%d: 2d-space: %ld - %ld / %ld - %ld\n", myid,
+    printf("T%d: 2d-space: %" PRId64 " - %" PRId64
+           " / %" PRId64 " - %" PRId64 "\n", myid,
            s->s.from.i[0], s->s.to.i[0], s->s.from.i[1], s->s.to.i[1]);
 
     s = laik_spacestore_get(i, "3d-space");
     assert(s && (s->dims == 3));
     assert(s->s.to.i[0] == 400);
-    printf("T%d: 3d-space: %ld - %ld / %ld - %ld / %ld - %ld\n", myid,
+    printf("T%d: 3d-space: %" PRId64 " - %" PRId64
+           " / %" PRId64 " - %" PRId64
+           " / %" PRId64 " - %" PRId64 "\n", myid,
            s->s.from.i[0], s->s.to.i[0],
            s->s.from.i[1], s->s.to.i[1],
            s->s.from.i[2], s->s.to.i[2]);
