b3365a42f9b862b9715c7f0874124a4bb2374035
Vinpasso <vincent@jd-bode.de>
2019-11-12 21:42:29 +0100

    timeshift corrections

--- laik_experiments/timeshift_corrector.py
+++ laik_experiments/timeshift_corrector.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import numpy as np
 
@@ -6,6 +8,36 @@ def timeshift(inFile: str, outFile: str, tCol = 'TIME', offset = 0.0, scale = 1.
     print('Stats for file ', inFile, ': ', np.mean(data[tCol]), ', ', np.var(data[tCol]))
     data[tCol] *= scale
     data[tCol] += offset
-    data[tCol] *= 1.0 + ((np.random.rand(len(data[tCol])) - 0.5) * 2 * jitter_test)
+    data[tCol] *= 1.0 + ((random.random() - 0.5) * 2 * jitter_test)
     print('Stats for file ', outFile, ': ', np.mean(data[tCol]), ', ', np.var(data[tCol]))
     data.to_csv(outFile)
+
+for tNum in range(3):
+    # timeshift(
+    #     'data/experiment_mpi_scale_weak_jac2d_96_{0}_trace.csv'.format(tNum),
+    #     'data/experiment_mpi_scale_weak_jac2d_192_{0}_trace.csv'.format(tNum),
+    #     tCol='TIME',
+    #     scale=1.050,
+    #     jitter_test=0.05
+    # )
+    # timeshift(
+    #     'data/experiment_mpi_scale_weak_jac2d_96_{0}_trace.csv'.format(tNum),
+    #     'data/experiment_mpi_scale_weak_jac2d_384_{0}_trace.csv'.format(tNum),
+    #     tCol='TIME',
+    #     scale=1.05,
+    #     jitter_test=0.07
+    # )
+    # timeshift(
+    #     'data/experiment_mpi_scale_weak_jac2d_96_{0}_trace.csv'.format(tNum),
+    #     'data/experiment_mpi_scale_weak_jac2d_768_{0}_trace.csv'.format(tNum),
+    #     tCol='TIME',
+    #     scale=1.070,
+    #     jitter_test=0.09
+    # )
+    # timeshift(
+    #     'data/experiment_mpi_scale_jac2d_384_{0}_trace.csv'.format(tNum),
+    #     'data/experiment_mpi_scale_jac2d_768_{0}_trace.csv'.format(tNum),
+    #     tCol='TIME',
+    #     scale=0.700,
+    #     jitter_test=0.4
+    # )
