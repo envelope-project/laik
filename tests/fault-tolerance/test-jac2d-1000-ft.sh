8d07b98792109eec74dc0933ee2f2352868b1d05
Vinpasso <vincent@jd-bode.de>
2019-12-07 16:08:23 +0100

    Update build systems for modified folder structure

--- tests/fault-tolerance/test-jac2d-1000-ft.sh
+++ tests/fault-tolerance/test-jac2d-1000-ft.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mpirun --oversubscribe -n 8 "../../tests/fault-tolerance/checkpoint-jac2d-recovery" --failureCheckFrequency 10 --checkpointFrequency 10 --redundancyCount 1 --rotationDistance 1 --plannedFailure 1 15 --plannedFailure 2 21  --plannedFailure 3 32 --plannedFailure 5 33 -s 1000 > test-jac2d-1000-ft.out
+EXECUTABLE="../../examples/fault-tolerance/checkpoint-jac2d-recovery"
+mpirun -v --oversubscribe -n 8 "$EXECUTABLE" --failureCheckFrequency 10 --checkpointFrequency 10 --redundancyCount 1 --rotationDistance 1 --plannedFailure 1 15 --plannedFailure 2 21  --plannedFailure 3 32 --plannedFailure 5 33 -s 1000 > test-jac2d-1000-ft.out
 cmp test-jac2d-1000-ft.out "$(dirname -- "${0}")/test-jac2d-1000-ft.expected"
