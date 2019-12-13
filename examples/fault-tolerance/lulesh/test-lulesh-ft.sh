9f1fd684838464dfbf77c2d4c712c9b63dc7ea5c
Vinpasso <vincent@jd-bode.de>
2019-12-13 17:29:48 +0100

    Remove laik lulesh from main repository

--- examples/fault-tolerance/lulesh/test-lulesh-ft.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export OMP_NUM_THREADS=1
-EXECUTABLE="./lulesh"
-mpirun --oversubscribe -n 8 "$EXECUTABLE" --failureCheckFrequency 5 --checkpointFrequency 10 --redundancyCount 1 --rotationDistance 1 --plannedFailure 1 32 -s 10 -i 50 -p -repart 1 > test-lulesh-ft.out
-cmp test-lulesh-ft.out "$(dirname -- "${0}")/test-lulesh-ft.expected"
