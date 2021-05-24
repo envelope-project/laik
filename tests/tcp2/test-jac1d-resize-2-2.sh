10c7af7337fbc5f6b3289d60e9b93c93ae13083c
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2021-05-25 01:12:59 +0200

    Add timeout to jac1d tests for TCP2 backend
    There are still same races which could lead to hangs...

--- tests/tcp2/test-jac1d-resize-2-2.sh
+++ tests/tcp2/test-jac1d-resize-2-2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-${LAUNCHER-./launcher} -n 2 -s 2 ../../examples/jac1d 100 50 -10 > test-jac1d-resize-2-2.out
+timeout 2 ./tcp2run -n 2 -s 2 ../../examples/jac1d 100 50 -10 > test-jac1d-resize-2-2.out
 cmp test-jac1d-resize-2-2.out "$(dirname -- "${0}")/test-jac1d-resize-2-2.expected"
--- tests/tcp2/test-jac1d-resize-4-r12.sh
+++ tests/tcp2/test-jac1d-resize-4-r12.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-${LAUNCHER-./launcher} -n 4 -r L[12] ../../examples/jac1d 100 50 -10 > test-jac1d-resize-4-r12.out
+timeout 2 ./tcp2run -n 4 -r L[12] ../../examples/jac1d 100 50 -10 > test-jac1d-resize-4-r12.out
 cmp test-jac1d-resize-4-r12.out "$(dirname -- "${0}")/test-jac1d-resize-4-r12.expected"
