f28da74111c11524230a9561c90f9820fc1ed826
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2021-05-28 01:30:18 +0200

    Tests: add own "mycmp" which prints diff on mismatch
    Only enabled on 2 tests for now...

--- tests/tcp2/test-resize-3-r1.sh
+++ tests/tcp2/test-resize-3-r1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 timeout 2 ./tcp2run -n 3 -r L1 ../../examples/resize 1 | LC_ALL='C' sort > test-resize-3-r1.out
-cmp test-resize-3-r1.out "$(dirname -- "${0}")/test-resize-3-r1.expected"
+./mycmp test-resize-3-r1.out "$(dirname -- "${0}")/test-resize-3-r1.expected"
