0e5f05230173aae33640ea9c902f2361567d3f4c
Robert Hubinger <rhubinger@t-online.de>
2022-06-08 15:13:32 +0200

    removed fixed shmem usage in min example

--- examples/min.c
+++ examples/min.c
@@ -1,10 +1,9 @@
 #include "laik.h"
 #include <stdio.h>
-#include "laik-backend-shmem.h"
 
 int main(int argc, char* argv[])
 {
-    Laik_Instance* instance = laik_init_shmem(&argc, &argv);
+    Laik_Instance* instance = laik_init(&argc, &argv);
     Laik_Group *world = laik_world(instance);
     int size = laik_size(world);
     int myid = laik_myid(world);
