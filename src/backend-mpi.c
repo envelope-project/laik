f4198d46c9621f03bd8f8971afc88e4e5a28fb4e
Robert Hubinger <ge35fot@mytum.de>
2022-09-02 15:49:33 +0200

    Added and updated copyright of new files

--- src/backend-mpi.c
+++ src/backend-mpi.c
@@ -1,6 +1,7 @@
 /*
  * This file is part of the LAIK library.
  * Copyright (c) 2017, 2018 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
+ * Copyright (c) 2022 Robert Hubinger <robert.hubinger@tum.de>
  *
  * LAIK is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
