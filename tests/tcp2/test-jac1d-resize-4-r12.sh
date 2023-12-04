6180851ef161b640f92088a3a2976f557f2785a6
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2023-12-04 22:49:36 +0100

    Allow 5 secs for tcp2 jac1d resize tests
    2s obviously is to short for free Github runner.
    And it also needs 2.6s on a 2 cores Intel 6300U laptop

--- tests/tcp2/test-jac1d-resize-4-r12.sh
+++ tests/tcp2/test-jac1d-resize-4-r12.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
-timeout 2 ./tcp2run -n 4 -r L[12] ../../examples/jac1d 100 50 -10 > test-jac1d-resize-4-r12.out
+timeout 5 ./tcp2run -n 4 -r L[12] ../../examples/jac1d 100 50 -10 > test-jac1d-resize-4-r12.out
 cmp test-jac1d-resize-4-r12.out "$(dirname -- "${0}")/test-jac1d-resize-4-r12.expected"
