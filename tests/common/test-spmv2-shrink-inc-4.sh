93f43d7915f4d17b843ebd89b974dded81b631f1
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2024-06-01 20:02:07 +0200

    speed up common spmv2 tests
    spmv2 uses OpenMP and defaults to number of cores for used threads
    per process. This unnecessarily oversubscribes the cores on tests
    with 4 processes.
    Avoid this by setting OMP_NUM_THREADS to 1 for these tests, resulting
    in faster tests.

--- tests/common/test-spmv2-shrink-inc-4.sh
+++ tests/common/test-spmv2-shrink-inc-4.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # test shrinking with incremental partitioner
-${LAUNCHER-./launcher} -n 4 ../../examples/spmv2 -s 2 -i 10 3000 | LC_ALL='C' sort > test-spmv2-shrink-inc-4.out
+OMP_NUM_THREADS=1 ${LAUNCHER-./launcher} -n 4 ../../examples/spmv2 -s 2 -i 10 3000 | LC_ALL='C' sort > test-spmv2-shrink-inc-4.out
 cmp test-spmv2-shrink-inc-4.out "$(dirname -- "${0}")/test-spmv2-4.expected"
