c0654707eff68dd5cb11ca470df26af82e00e2ca
Alisa Parashchenko <ge24cuc@mytum.de>
2024-09-14 15:30:08 +0200

    Add code to measure how long resize takes

--- examples/resize.c
+++ examples/resize.c
@@ -24,15 +24,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
+const char* usage = "\
+Usage: %s [-t] <maxiter>\n\
+-t: measure and print how long each resize took\n";
+
 int main(int argc, char* argv[])
 {
-    Laik_Instance* inst = laik_init(&argc, &argv);
-    int phase = laik_phase(inst);
-
     int max = 0;
+    int timings = 0;
+
+    // Option to measure how long the resize took
+    if (argc > 1 && argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 't': timings = 1; break;
+            case 'h': printf(usage, argv[0]); return 0;
+            default:
+              fprintf(stderr, "Unrecognized option -%c\n", argv[1][1]);
+              return 1;
+        }
+        argc--;
+        argv++;
+    }
+
     if (argc > 1) max = atoi(argv[1]);
     if (max == 0) max = 10;
 
+    Laik_Instance* inst = laik_init(&argc, &argv);
+    int phase = laik_phase(inst);
+
+    double start_time, end_time;
     Laik_Group *world = laik_world(inst);
     while(1) {
         printf("Epoch %d / Phase %d: Hello from process %d of %d\n",
@@ -45,9 +65,13 @@ int main(int argc, char* argv[])
         phase++;
 
         // allow resize of world and get new world
+        start_time = laik_wtime();
         world = laik_allow_world_resize(inst, phase);
+        end_time = laik_wtime();
 
         if (laik_myid(world) < 0) break;
+        if (timings) printf("%d: resize took %f msec\n",
+                            laik_myid(world), end_time - start_time);
     }
 
     laik_finalize(inst);
